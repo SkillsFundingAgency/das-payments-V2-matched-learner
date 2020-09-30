5a24d336f71d8b2d23752825da33026bc02bae15
Tom <Tom>
2020-09-30 15:02:28 +0100

    Config added

--- MatchedLearnerApi/Program.cs
+++ MatchedLearnerApi/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
+using SFA.DAS.Configuration.AzureTableStorage;
 
 namespace MatchedLearnerApi
 {
@@ -16,11 +20,18 @@ namespace MatchedLearnerApi
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    var environmentName = hostingContext.HostingEnvironment.EnvironmentName;
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appSettings.json", optional: false, reloadOnChange: false);
+                    config.AddJsonFile($"appSettings.{environmentName}.json", optional: true, reloadOnChange: false);
+                    config.AddEnvironmentVariables();
+                })
+                .UseUrls("https://localhost:5061")
+                .UseNLog();
     }
 }
