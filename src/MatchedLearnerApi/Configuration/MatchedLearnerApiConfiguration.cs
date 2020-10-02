f2e9c33fe9184d35a9696b95b20bf149b935e02c
Akshay Chugh <akshay.chugh30@outlook.com>
2020-10-02 17:31:30 +0100

    PV2-2130 Configuration object + interface added

---
+++ src/MatchedLearnerApi/Configuration/MatchedLearnerApiConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatchedLearnerApi.Interfaces;
+
+namespace MatchedLearnerApi.Configuration
+{
+    public class MatchedLearnerApiConfiguration : IMatchedLearnerApiConfiguration
+    {
+        public string DasPaymentsDatabaseConnectionString { get; set; }
+        public string LoggingConnectionString { get; set; }
+        public string LoggingKey { get; set; }
+    }
+}
---
+++ src/MatchedLearnerApi/Interfaces/IMatchedLearnerApiConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchedLearnerApi.Interfaces
+{
+    public interface IMatchedLearnerApiConfiguration
+    {
+        string DasPaymentsDatabaseConnectionString { get; set; }
+        string LoggingConnectionString { get; set; }
+        string LoggingKey { get; set; }
+    }
+}
