82c8ea1cc3c60d52e5fcaf01c649d24968dea912
Akshay Chugh <akshay.chugh30@outlook.com>
2020-10-02 17:29:47 +0100

    PV2-2130 Added AzureTableStorage to config list

---
+++ src/MatchedLearnerApi/Configuration/MatchedLearnerApiConfigurationKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchedLearnerApi.Configuration
+{
+    public class MatchedLearnerApiConfigurationKeys
+    {
+        public const string MatchedLearnerApi = "SFA.DAS.MatchedLearnerApi";
+    }
+}
