eb8c6e0fb5cb4afafaedbc9ee5065e3fbe171e9a
Carl <carl@techcellence.org.uk>
2020-10-07 12:42:16 +0100

    Added initial framework for smoke tests

---
+++ src/MatchedLearnerApi.AcceptanceTests/Request.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MatchedLearnerApi.Types;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace MatchedLearnerApi.AcceptanceTests
+{
+    public class Request
+    {
+        private static readonly HttpClient Client = new HttpClient();
+        private readonly string _url;
+
+        public Request()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddEnvironmentVariables();
+            configurationBuilder.SetBasePath(System.IO.Directory.GetCurrentDirectory());
+            configurationBuilder.AddJsonFile("appsettings.json");
+
+            var configuration = configurationBuilder.Build();
+
+            _url = configuration["TargetUrl"];
+        }
+
+        public async Task<MatchedLearnerResultDto> Handle(long ukprn, long uln)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, _url + $"/api/v1/{ukprn}/{uln}");
+            
+            try
+            {
+                var response = await Client.SendAsync(request);
+
+                if(!response.IsSuccessStatusCode)
+                    throw new Exception($"{(int)response.StatusCode}");
+
+                var responseAsString = await response.Content.ReadAsStringAsync();
+                var responseAsObjectGraph = JsonConvert.DeserializeObject<MatchedLearnerResultDto>(responseAsString);
+
+                return responseAsObjectGraph;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ src/MatchedLearnerApi.AcceptanceTests/SmokeTests/RequestToGet404Works.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MatchedLearnerApi.Types;
+using NUnit.Framework;
+
+namespace MatchedLearnerApi.AcceptanceTests.SmokeTests
+{
+    [TestFixture]
+    public class RequestToGet404Works
+    {
+        [Test]
+        public async Task RequestWithBadData_Should_Return404()
+        {
+            var request = new Request();
+            Func<Task> act = request.Awaiting(async x => await x.Handle(0, 0));
+            act.Should().Throw<Exception>()
+                .WithMessage("404");
+        }
+    }
+}
