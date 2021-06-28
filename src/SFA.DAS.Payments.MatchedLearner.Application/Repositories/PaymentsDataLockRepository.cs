770289f386b77a2aebe1d66f4bd21a045e1fdc5c
Hardik Desai <hardik.desai@digital.education.gov.uk>
2021-06-28 12:11:25 +0100

    get all academic year data in single query

--- src/SFA.DAS.Payments.MatchedLearner.Application/Repositories/PaymentsDataLockRepository.cs
+++ src/SFA.DAS.Payments.MatchedLearner.Application/Repositories/PaymentsDataLockRepository.cs
@@ -29,87 +29,77 @@ namespace SFA.DAS.Payments.MatchedLearner.Application.Repositories
         public async Task<MatchedLearnerDataLockInfo> GetDataLockEvents(long ukprn, long uln)
         {
             var stopwatch = Stopwatch.StartNew();
-            var result = new MatchedLearnerDataLockInfo
-            {
-                DataLockEvents = new List<DataLockEvent>(),
-                DataLockEventPriceEpisodes = new List<DataLockEventPriceEpisode>(),
-                DataLockEventPayablePeriods = new List<DataLockEventPayablePeriod>(),
-                DataLockEventNonPayablePeriods = new List<DataLockEventNonPayablePeriod>(),
-                DataLockEventNonPayablePeriodFailures = new List<DataLockEventNonPayablePeriodFailure>(),
-                Apprenticeships = new List<Apprenticeship>()
-            };
 
-            var academicYears = _context.LatestSuccessfulJobs
+            var latestSuccessfulJobs = _context.LatestSuccessfulJobs
                 .Where(x => x.Ukprn == ukprn)
-                .Select(x => x.AcademicYear)
                 .OrderByDescending(x => x)
+                .Select(l => l.DcJobId)
                 .Distinct()
                 .ToList();
 
-            foreach (var academicYear in academicYears)
+            var transactionTypes = new List<byte> { 1, 2, 3 };
+
+            var dataLockEvents = await _context.DataLockEvent
+                .Where(x =>
+                    x.LearningAimReference == "ZPROG001" &&
+                    x.Ukprn == ukprn &&
+                    x.LearnerUln == uln &&
+                    latestSuccessfulJobs.Contains(x.JobId))
+                .OrderBy(x => x.LearningStartDate)
+                .ToListAsync();
+
+            var eventIds = dataLockEvents.Select(d => d.EventId).ToList();
+
+            var dataLockEventPriceEpisodes = await _context.DataLockEventPriceEpisode
+                .Where(d => eventIds.Contains(d.DataLockEventId) && d.PriceEpisodeIdentifier != null)
+                .OrderBy(p => p.StartDate)
+                .ThenBy(p => p.PriceEpisodeIdentifier)
+                .ToListAsync();
+
+            var dataLockEventPayablePeriods = await _context.DataLockEventPayablePeriod
+                .Where(d => eventIds.Contains(d.DataLockEventId) && transactionTypes.Contains(d.TransactionType) && d.PriceEpisodeIdentifier != null && d.Amount != 0)
+                .OrderBy(p => p.DeliveryPeriod)
+                .ToListAsync();
+
+            var dataLockEventNonPayablePeriods = await _context.DataLockEventNonPayablePeriod
+                .Where(d => eventIds.Contains(d.DataLockEventId) && transactionTypes.Contains(d.TransactionType) && d.PriceEpisodeIdentifier != null && d.Amount != 0)
+                .OrderBy(p => p.DeliveryPeriod)
+                .ToListAsync();
+
+            var dataLockEventNonPayablePeriodIds = dataLockEventNonPayablePeriods.Select(d => d.DataLockEventNonPayablePeriodId).ToList();
+
+            var dataLockEventNonPayablePeriodFailures = new List<DataLockEventNonPayablePeriodFailure>();
+            if (dataLockEventNonPayablePeriodIds.Any())
             {
-                var latestSuccessfulJob = _context.LatestSuccessfulJobs
-                    .Where(y => y.Ukprn == ukprn)
-                    .Where(y => y.AcademicYear == academicYear)
-                    .OrderByDescending(y => y.CollectionPeriod)
-                    .First();
-
-                _logger.LogDebug($"Getting DataLock Event Data Uln: {uln}, Academic year: {latestSuccessfulJob.AcademicYear}, Collection period: {latestSuccessfulJob.CollectionPeriod}");
-
-                var transactionTypes = new List<byte> { 1, 2, 3 };
-
-                var dataLockEvents = await _context.DataLockEvent
-                    .Where(x =>
-                        x.LearningAimReference == "ZPROG001" &&
-                        x.Ukprn == ukprn &&
-                        x.LearnerUln == uln &&
-                        x.JobId == latestSuccessfulJob.DcJobId &&
-                        x.AcademicYear == latestSuccessfulJob.AcademicYear &&
-                        x.CollectionPeriod == latestSuccessfulJob.CollectionPeriod)
-                    .OrderBy(x => x.LearningStartDate)
-                    .ToListAsync();
-
-                var eventIds = dataLockEvents.Select(d => d.EventId).ToList();
-
-                var dataLockEventPriceEpisodes = await _context.DataLockEventPriceEpisode
-                    .Where(d => eventIds.Contains(d.DataLockEventId) && d.PriceEpisodeIdentifier != null)
-                    .OrderBy(p => p.StartDate)
-                    .ToListAsync();
-
-                var dataLockEventPayablePeriods = await _context.DataLockEventPayablePeriod
-                    .Where(d => eventIds.Contains(d.DataLockEventId) && transactionTypes.Contains(d.TransactionType) && d.PriceEpisodeIdentifier != null && d.Amount != 0)
-                    .OrderBy(p => p.DeliveryPeriod)
-                    .ToListAsync();
-
-                var dataLockEventNonPayablePeriods = await _context.DataLockEventNonPayablePeriod
-                    .Where(d => eventIds.Contains(d.DataLockEventId) && transactionTypes.Contains(d.TransactionType) && d.PriceEpisodeIdentifier != null && d.Amount != 0)
-                    .OrderBy(p => p.DeliveryPeriod)
-                    .ToListAsync();
-
-                var dataLockEventNonPayablePeriodIds = dataLockEventNonPayablePeriods.Select(d => d.DataLockEventNonPayablePeriodId).ToList();
-
-                var dataLockEventNonPayablePeriodFailures = await _context.DataLockEventNonPayablePeriodFailures
+                dataLockEventNonPayablePeriodFailures = await _context.DataLockEventNonPayablePeriodFailures
                     .Where(d => dataLockEventNonPayablePeriodIds.Contains(d.DataLockEventNonPayablePeriodId))
                     .ToListAsync();
-
-                var apprenticeshipIds = dataLockEventPayablePeriods.Select(d => d.ApprenticeshipId)
-                     .Union(dataLockEventNonPayablePeriodFailures.Select(d => d.ApprenticeshipId))
-                     .Distinct()
-                     .ToList();
-
-                var apprenticeshipDetails = await _context.Apprenticeship.Where(a => apprenticeshipIds.Contains(a.Id)).ToListAsync();
-
-                result.DataLockEvents.AddRange(dataLockEvents);
-                result.DataLockEventPriceEpisodes.AddRange(dataLockEventPriceEpisodes);
-                result.DataLockEventPayablePeriods.AddRange(dataLockEventPayablePeriods);
-                result.DataLockEventNonPayablePeriods.AddRange(dataLockEventNonPayablePeriods);
-                result.DataLockEventNonPayablePeriodFailures.AddRange(dataLockEventNonPayablePeriodFailures);
-                result.Apprenticeships.AddRange(apprenticeshipDetails);
             }
+
+            var apprenticeshipIds = dataLockEventPayablePeriods.Select(d => d.ApprenticeshipId)
+                 .Union(dataLockEventNonPayablePeriodFailures.Select(d => d.ApprenticeshipId))
+                 .Distinct()
+                 .ToList();
+
+            var apprenticeshipDetails = new List<Apprenticeship>();
+            if (apprenticeshipIds.Any())
+            {
+                apprenticeshipDetails = await _context.Apprenticeship.Where(a => apprenticeshipIds.Contains(a.Id)).ToListAsync();
+            }
+
+            var result = new MatchedLearnerDataLockInfo
+            {
+                DataLockEvents = dataLockEvents,
+                DataLockEventPriceEpisodes = dataLockEventPriceEpisodes,
+                DataLockEventPayablePeriods = dataLockEventPayablePeriods,
+                DataLockEventNonPayablePeriods = dataLockEventNonPayablePeriods,
+                DataLockEventNonPayablePeriodFailures = dataLockEventNonPayablePeriodFailures,
+                Apprenticeships = apprenticeshipDetails
+            };
 
             stopwatch.Stop();
 
-            _logger.LogInformation($"Finished getting DataLock Event Data Duration: {stopwatch.ElapsedMilliseconds} Uln: {uln}, Academic years covered: {string.Join(",", academicYears)}");
+            _logger.LogInformation($"Finished getting DataLock Event Data Duration: {stopwatch.ElapsedMilliseconds} Uln: {uln}");
 
             return result;
         }
