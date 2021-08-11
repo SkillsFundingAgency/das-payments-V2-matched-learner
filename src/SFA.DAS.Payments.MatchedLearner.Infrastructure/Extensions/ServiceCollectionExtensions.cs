b46797175fcd25cb44a68b4652b86bfe6b34984d
Hardik Desai <hardik.desai@digital.education.gov.uk>
2021-08-11 09:26:18 +0100

    updated azure template with all the correct variables and added missing variables

--- src/SFA.DAS.Payments.MatchedLearner.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.Payments.MatchedLearner.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -63,6 +63,7 @@ namespace SFA.DAS.Payments.MatchedLearner.Infrastructure.Extensions
                 {
                     options.PreFixConfigurationKeys = false;
                     options.ConfigurationKeys = new[] { ApplicationSettingsKeys.MatchedLearnerApiKey };
+                    options.StorageConnectionStringEnvironmentVariableName = "AzureWebJobsStorage";
                 });
             }
 #if DEBUG
