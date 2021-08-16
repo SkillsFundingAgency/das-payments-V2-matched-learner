5e4aee215e58f872051315a3765fb72ea613189a
Tom <Tom>
2021-08-16 11:19:19 +0100

    Schema Updates

--- src/SFA.DAS.Payments.MatchedLearner.Database/dbo/Tables/dbo.Training.sql
+++ src/SFA.DAS.Payments.MatchedLearner.Database/dbo/Tables/dbo.Training.sql
@@ -1,19 +1,18 @@
 ﻿CREATE TABLE [dbo].[Training]
 (
 	[Id] BIGINT NOT NULL PRIMARY KEY, 
-    [StartDate] DATETIMEOFFSET NOT NULL,
 	[EventTime] DATETIMEOFFSET NOT NULL,
 	[IlrSubmissionDate] DATETIMEOFFSET NOT NULL, 
     [IlrSubmissionWindowPeriod] INT NOT NULL,
 	[AcademicYear] INT NOT NULL,
 	[Ukprn] BIGINT NOT NULL,
 	[Uln] BIGINT NOT NULL,
-	[Reference] NVARCHAR(50) NOT NULL,
+	[Reference] NVARCHAR(8) NOT NULL,
 	[ProgrammeType] INT NOT NULL,
 	[StandardCode] INT NOT NULL,
 	[FrameworkCode] INT NOT NULL,
 	[PathwayCode] INT NOT NULL,
 	[FundingLineType] NVARCHAR(100) NOT NULL,
-	[TrainingStartDate] DATETIME NOT NULL,
+	[StartDate] DATETIME NOT NULL,
 	[CreationDate]  DATETIMEOFFSET NOT NULL CONSTRAINT DF_Training__CreationDate DEFAULT (SYSDATETIMEOFFSET()),
 )
