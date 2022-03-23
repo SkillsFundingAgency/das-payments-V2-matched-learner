5596b1fa14907bb2b014602ebadcd99342355e19
Hardik Desai <hardik.desai@education.gov.uk>
2022-03-23 11:58:20 +0000

    added script for EI test client tables

---
+++ src/SFA.DAS.Payments.EI.TestClient/Deploy EI Test Client Tables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE [Payments2].[ApprenticeshipInput](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Ukprn] [bigint] NOT NULL,
+	[Uln] [bigint] NOT NULL,
+	[CreationDate] [datetimeoffset](7) NOT NULL,
+ CONSTRAINT [PK_ApprenticeshipInput] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Payments2].[ApprenticeshipInput] ADD  CONSTRAINT [DF_ApprenticeshipInput__CreationDate]  DEFAULT (sysdatetimeoffset()) FOR [CreationDate]
+GO
+
+
+CREATE TABLE [Payments2].[ApprenticeshipOutPut](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Ukprn] [bigint] NOT NULL,
+	[Uln] [bigint] NOT NULL,
+	[CreationDate] [datetimeoffset](7) NOT NULL,
+ CONSTRAINT [PK_ApprenticeshipOutPut] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [Payments2].[ApprenticeshipOutPut] ADD  CONSTRAINT [DF_ApprenticeshipOutPut__CreationDate]  DEFAULT (sysdatetimeoffset()) FOR [CreationDate]
+GO
+
+
