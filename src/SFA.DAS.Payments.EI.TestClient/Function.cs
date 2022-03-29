a224a936db1a521d2dc2afa9a10244b0769a5ff3
Hardik Desai <hardik.desai@education.gov.uk>
2022-03-29 16:03:37 +0100

    added new task for second call and moved the db call into main activity

--- src/SFA.DAS.Payments.EI.TestClient/Function.cs
+++ src/SFA.DAS.Payments.EI.TestClient/Function.cs
@@ -72,9 +72,30 @@ namespace SFA.DAS.Payments.EI.TestClient
 
     public class LearnerMatchingApprenticeshipOrchestrator
     {
-        private readonly TestDataContext _dataContext;
-        public LearnerMatchingApprenticeshipOrchestrator(IConfiguration configuration)
+        [FunctionName(nameof(LearnerMatchingApprenticeshipOrchestrator))]
+        public async Task RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
         {
+            var incentive = context.GetInput<ApprenticeshipInput>();
+
+            await context.CallActivityWithRetryAsync(nameof(LearnerMatchAndUpdate), new RetryOptions(TimeSpan.FromSeconds(1), 3), incentive);
+
+            var deadline = context.CurrentUtcDateTime.Add(TimeSpan.FromMilliseconds(100));
+            await context.CreateTimer(deadline, CancellationToken.None);
+
+            await context.CallActivityWithRetryAsync(nameof(LearnerMatchAndUpdate2), new RetryOptions(TimeSpan.FromSeconds(1), 3), incentive);
+        }
+    }
+
+
+    public class LearnerMatchAndUpdate
+    {
+        private readonly HttpClient _client;
+        private readonly TestDataContext _dataContext;
+
+        public LearnerMatchAndUpdate(IConfiguration configuration, HttpClient client)
+        {
+            _client = client;
+
             var connectionString = configuration["ConnectionString"];
 
             if (string.IsNullOrEmpty(connectionString))
@@ -85,42 +106,47 @@ namespace SFA.DAS.Payments.EI.TestClient
                 .Options;
 
             _dataContext = new TestDataContext(options);
-
         }
 
-        [FunctionName(nameof(LearnerMatchingApprenticeshipOrchestrator))]
-        public async Task RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        [FunctionName(nameof(LearnerMatchAndUpdate))]
+        public async Task Create([ActivityTrigger] ApprenticeshipInput apprenticeshipInput)
         {
-            var incentive = context.GetInput<ApprenticeshipInput>();
+            var response = await _client.GetAsync($"api/v1/{apprenticeshipInput.Ukprn}/{apprenticeshipInput.Uln}?");
 
-            var apprenticeshipOutPutResult = await context.CallActivityWithRetryAsync<ApprenticeshipOutPut>(nameof(LearnerMatchAndUpdate),
-                new RetryOptions(TimeSpan.FromSeconds(1), 3),
-                incentive);
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return;
+            }
 
+            response.EnsureSuccessStatusCode();
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            var data = JsonConvert.DeserializeObject<MatchedLearnerDto>(jsonString);
+
+            if (data == null || data.Uln != apprenticeshipInput.Uln)
+            {
+                throw new InvalidOperationException(jsonString);
+            }
+
+            var apprenticeshipOutPutResult = new ApprenticeshipOutPut { Ukprn = apprenticeshipInput.Ukprn, Uln = apprenticeshipInput.Uln, LearnerJson = jsonString };
+            
             await _dataContext.ApprenticeshipOutPut.AddAsync(apprenticeshipOutPutResult);
 
             await _dataContext.SaveChangesAsync();
 
-            var deadline = context.CurrentUtcDateTime.Add(TimeSpan.FromMilliseconds(100));
-            await context.CreateTimer(deadline, CancellationToken.None);
-
-            await context.CallActivityWithRetryAsync<ApprenticeshipOutPut>(nameof(LearnerMatchAndUpdate),
-                new RetryOptions(TimeSpan.FromSeconds(1), 3),
-                incentive);
         }
     }
-
-
-    public class LearnerMatchAndUpdate
+    public class LearnerMatchAndUpdate2
     {
         private readonly HttpClient _client;
 
-        public LearnerMatchAndUpdate(HttpClient client)
+        public LearnerMatchAndUpdate2(HttpClient client)
         {
             _client = client;
         }
 
-        [FunctionName(nameof(LearnerMatchAndUpdate))]
+        [FunctionName(nameof(LearnerMatchAndUpdate2))]
         public async Task<ApprenticeshipOutPut> Create([ActivityTrigger] ApprenticeshipInput apprenticeshipInput)
         {
             var response = await _client.GetAsync($"api/v1/{apprenticeshipInput.Ukprn}/{apprenticeshipInput.Uln}?");
